f04560016e7e38ab78681eaf30e708243cada06e
16ur <akuseruuu@protonmail.com>
2024-12-05 20:21:42 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from "react";
 import { Analytics } from "@vercel/analytics/react";
-<<<<<<< HEAD
-=======
-import reactLogo from "./assets/react.svg";
->>>>>>> 3246522 (About me in process, save before restyle)
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import CoreContent from "./components/CoreContent/CoreContent";
