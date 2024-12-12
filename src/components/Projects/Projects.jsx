e32e3f24ba2ae1ed8ec0472f54a0ab02119aaaa5
16ur <akuseruuu@protonmail.com>
2024-12-12 11:57:09 +0100

    Fix soucis de CSS

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,15 @@
+import React, { useState } from "react";
 import "./Projects.css";
 
 function Projects() {
+  const [showModal, setShowModal] = useState(false);
+
   const projects = [
     {
       id: 1,
       title: "Portfolio Personnel",
       description:
         "Mon site web personnel réalisé avec ReactJS, mettant en valeur mes compétences et mes projets.",
-      image: "path/to/project1.jpg",
       link: "https://github.com/16ur/myPF",
     },
     {
@@ -15,19 +17,28 @@ function Projects() {
       title: "KeyCrafted",
       description:
         "Réalisé dans le cadre de ma formation, KeyCrafted est un site web de vente de claviers et d'accessoires pour les passionnés de claviers. (Projet en cours)",
-
-      image: "path/to/project2.jpg",
       link: "https://github.com/16ur/keycrafted",
     },
     {
       id: 3,
       title: "Finder",
       description:
-        "Réalisé dans le cadre de ma formation, Finder est un site web réalisé à plusieurs, permettant de mettre en relation des particuliers et des professionnels pour des services.",
-      image: "path/to/project3.jpg",
-      link: "https://github.com/ecommercefrontend",
+        "Réalisé dans le cadre de ma formation, Finder est un site web réalisé en groupe, permettant de mettre en relation des particuliers et des professionnels afin de rendre des services.",
+      link: null,
     },
   ];
+
+  const handleLinkClick = (link) => {
+    if (!link) {
+      setShowModal(true);
+    } else {
+      window.open(link, "_blank", "noopener,noreferrer");
+    }
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <div className="projectsPage">
@@ -40,18 +51,32 @@ function Projects() {
             <div className="projectContent">
               <h2 className="projectTitle">{project.title}</h2>
               <p className="projectDescription">{project.description}</p>
-              <a
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
+              <button
                 className="projectLink"
+                onClick={() => handleLinkClick(project.link)}
               >
                 Voir le projet
-              </a>
+              </button>
             </div>
           </div>
         ))}
       </div>
+
+      {showModal && (
+        <div className="modalBackdrop" onClick={closeModal}>
+          <div className="modalContent" onClick={(e) => e.stopPropagation()}>
+            <h2>Projet non disponible</h2>
+            <p>
+              Ce projet est académique et ne peut pas être divulgué pour le
+              moment. <br />
+              <br /> Merci de votre compréhension.
+            </p>
+            <button className="closeButton" onClick={closeModal}>
+              Fermer
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
