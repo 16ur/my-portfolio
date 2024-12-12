336e9843a08b4624a51417ea553429f07dbe1609
16ur <akuseruuu@protonmail.com>
2024-12-12 15:13:01 +0100

    Fix mail and phone number

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,10 +14,10 @@ function Contact() {
           <h2>Mes coordonnées</h2>
           <p>
             Email :{" "}
-            <a href="mailto:example@example.com">axelmanguian@gmail.com</a>
+            <a href="mailto:axelmanguian@gmail.com">axelmanguian@gmail.com</a>
           </p>
           <p>
-            Téléphone : <a href="tel:+123456789">+33 7 83 51 51 41</a>
+            Téléphone : <a href="tel:+33783515141">+33 7 83 51 51 41</a>
           </p>
           <p>Réseaux sociaux :</p>
           <div className="socialIcons">
